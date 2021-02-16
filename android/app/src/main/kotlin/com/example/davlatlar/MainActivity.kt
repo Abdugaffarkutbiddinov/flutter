7a8b1341dc379eaeb03995e02a6b38b018646c9a
Abdugaffarkutbiddinov <Abdugaffarkutbiddinov@gmail.com>
2021-02-16 14:41:07 +0300

    Davlatlar

---
+++ android/app/src/main/kotlin/com/example/davlatlar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.davlatlar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:davlatlar/screens/AllCountries.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: AllCountries(),
+  ));
+}
